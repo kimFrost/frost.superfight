e2e83733045fce793d4a23bcc5f8fc19cc6d21fa
Kim Frost <kim@noerd.dk>
2015-03-06 13:38:11 +0100

    ()

--- app.js
+++ app.js
@@ -19,12 +19,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -33,17 +27,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 /*
 app.use('/', index);
 app.use('/users', users);
 */
-
 
 /*
 app.get('/', taskList.showTasks.bind(taskList));
 app.post('/addtask', taskList.addTask.bind(taskList));
 app.post('/completetask', taskList.completeTask.bind(taskList));
 */
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
