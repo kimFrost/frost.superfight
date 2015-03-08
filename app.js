9b2174a8e27cbd2c190504232a58e7ce1103cefc
Kim Frost <kim@thatworks.dk>
2015-03-08 12:25:52 +0100

    ()

--- app.js
+++ app.js
@@ -45,7 +45,12 @@ app.get('/', taskList.showTasks.bind(taskList));
 // POST
 app.post('/addtask', taskList.addTask.bind(taskList));
 app.post('/completetask', taskList.completeTask.bind(taskList));
-app.post('/addcard', taskList.addCard.bind(taskList));
+//app.post('/addcard', taskList.addCard.bind(taskList));
+
+
+app.get('/addcard', function(req, res) {
+	res.send('blahh');
+});
 
 
 
